097ef1c0fdfbefa1d842e6da4677ef6f5d85fb3f
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-11 15:55:26 -0500

    Remove header code from implementation

--- Tutorial/Tutorial/sender.cc
+++ Tutorial/Tutorial/sender.cc
@@ -1,29 +1,4 @@
-#include <cstdint>
-#include <stddef.h>
-#include <stdio.h>
-//#include <winsock2.h>
-
-#define DEV_ADDR_0 "192.168.1.77"
-#define DEV_ADDR_1 "192.168.1.78"
-#define PORT 9591
-
-struct target {
-  static const size_t SIZE
-    = 2 * sizeof(uint16_t)
-    + 7 * sizeof(double);
-
-  uint16_t cam_id;
-  uint16_t target_id;
-
-  double pos_x;
-  double pos_y;
-  double pos_z;
-
-  double rot_x;
-  double rot_y;
-  double rot_z;
-  double rot_w;
-};
+#include "sender.h"
 
 int serialize(const target &data, char *buf) {
   uint16_t *ids = (uint16_t*)buf;
@@ -64,13 +39,6 @@ int deserialize(char *buf, target &data) {
 
   return 0;
 }
-
-struct sender {
-  sockaddr_in dev0, dev1;
-  WSADATA wsa;
-  int sock;
-  size_t devlen;
-};
 
 int initSender(sender &s) {
   if (WSAStartup(MAKEWORD(2, 2), &s.wsa) != 0) {
