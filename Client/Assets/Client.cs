229c408ad3eda6a98c1d92b9fd2fa90f8e165c1e
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-13 16:48:29 -0500

    Add Listener instance to Client

--- Client/Assets/Client.cs
+++ Client/Assets/Client.cs
@@ -1,66 +1,12 @@
-using System;
-using System.Net;
-using System.Net.Sockets;
 using UnityEngine;
 
 public class Client : MonoBehaviour {
 
   public short port = 9591;
 
-  Socket sock = null;
-  IPEndPoint end = null;
+  Listener listener;
 
   void Awake() {
-    /* Create */
-
-    try {
-      sock = new Socket(
-        AddressFamily.InterNetwork,
-        SocketType.Dgram,
-        ProtocolType.Udp
-      );
-    } catch (Exception e) {
-      Debug.LogWarning(e);
-      return;
-    }
-
-    Debug.Log("Created socket with handle " + sock.Handle);
-
-    /* Bind */
-
-    try {
-      end = new IPEndPoint(
-        IPAddress.Any,
-        port
-      );
-    } catch (Exception e) {
-      Debug.LogWarning(e);
-      return;
-    }
-
-    try {
-      sock.Bind(end);
-    } catch (Exception e) {
-      Debug.LogWarning(e);
-      return;
-    }
-
-    Debug.Log("Bound socket to " + end);
-  }
-
-  void Run() {
-    byte[] buffer = new byte[Target.SIZE];
-
-    try {
-      // Get buffer (blocking)
-      if (sock.Receive(buffer) == 0) return;
-    } catch (Exception e) {
-      Debug.LogWarning(e);
-      return;
-    }
-
-    Target target = Target.Deserialize(buffer);
-
-    Debug.Log(target);
+    listener = new Listener(port);
   }
 }
