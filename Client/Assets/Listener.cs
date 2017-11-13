6f02bc3387d2c8bb19d9ff605110485b8a228af2
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-13 16:48:20 -0500

    Move socket code to Listener class

---
+++ Client/Assets/Listener.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Listener {
+
+  Socket sock = null;
+  IPEndPoint end = null;
+
+  public Listener(short port) {
+    /* Create */
+
+    try {
+      sock = new Socket(
+        AddressFamily.InterNetwork,
+        SocketType.Dgram,
+        ProtocolType.Udp
+      );
+    } catch (Exception e) {
+      Debug.LogWarning(e);
+      return;
+    }
+
+    Debug.Log("Created socket with handle " + sock.Handle);
+
+    /* Bind */
+
+    try {
+      end = new IPEndPoint(
+        IPAddress.Any,
+        port
+      );
+    } catch (Exception e) {
+      Debug.LogWarning(e);
+      return;
+    }
+
+    try {
+      sock.Bind(end);
+    } catch (Exception e) {
+      Debug.LogWarning(e);
+      return;
+    }
+
+    Debug.Log("Bound socket to " + end);
+  }
+
+  void Run() {
+    byte[] buffer = new byte[Target.SIZE];
+
+    try {
+      // Get buffer (blocking)
+      if (sock.Receive(buffer) == 0) return;
+    } catch (Exception e) {
+      Debug.LogWarning(e);
+      return;
+    }
+
+    Target target = Target.Deserialize(buffer);
+
+    Debug.Log(target);
+  }
+}
