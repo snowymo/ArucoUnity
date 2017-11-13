69d408306c89ba0d1431071a187db0d64825d4bb
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-13 16:12:35 -0500

    Add Deserialize() method

--- Client/Assets/Target.cs
+++ Client/Assets/Target.cs
@@ -1,3 +1,5 @@
+using System;
+
 public struct Target {
 
   public const int SIZE
@@ -15,4 +17,35 @@ public struct Target {
   double rot_y;
   double rot_z;
   double rot_w;
+
+  public void Deserialize(byte[] buffer) {
+    int index = 0;
+
+    this.cam_id = BitConverter.ToUInt16(buffer, index);
+    index += sizeof(ushort);
+
+    this.cam_id = BitConverter.ToUInt16(buffer, index);
+    index += sizeof(ushort);
+
+    /* Position */
+
+    this.pos_x = BitConverter.ToDouble(buffer, index);
+    index += sizeof(double);
+
+    this.pos_y = BitConverter.ToDouble(buffer, index);
+    index += sizeof(double);
+
+    this.pos_z = BitConverter.ToDouble(buffer, index);
+    index += sizeof(double);
+
+    /* Rotation */
+
+    this.rot_x = BitConverter.ToDouble(buffer, index);
+    index += sizeof(double);
+
+    this.rot_y = BitConverter.ToDouble(buffer, index);
+    index += sizeof(double);
+
+    this.rot_z = BitConverter.ToDouble(buffer, index);
+  }
 }
