5dbf99a6d1b73e5cf21dc8e82d5300ff78d6dd28
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-15 16:05:39 -0500

    Use position and rotation getters in TrackedObject

--- Client/Assets/Scripts/TrackedObject.cs
+++ Client/Assets/Scripts/TrackedObject.cs
@@ -12,17 +12,7 @@ public class TrackedObject : MonoBehaviour {
     var pair = new KeyValuePair<int, int>(CAMERA_ID, id);
     Target target = client.listener.GetTarget(pair);
 
-    transform.position = new Vector3(
-      (float)target.pos_x,
-      (float)target.pos_y,
-      (float)target.pos_z
-    );
-
-    transform.rotation = new Quaternion(
-      (float)target.rot_x,
-      (float)target.rot_y,
-      (float)target.rot_z,
-      (float)target.rot_w
-    );
+    transform.position = target.GetPosition();
+    transform.rotation = target.GetRotation();
   }
 }
