ee7b3e39008b9988ad7cc0c479a494ae5e297bed
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-15 16:42:42 -0500

    Calculate target delta time in TrackedObject

--- Client/Assets/Scripts/TrackedObject.cs
+++ Client/Assets/Scripts/TrackedObject.cs
@@ -8,11 +8,22 @@ public class TrackedObject : MonoBehaviour {
   public Client client;
   public int id;
 
+  Target lastTarget;
+  float deltaTime = 0;
+
   void Update() {
+    // Get target from listener with camera and target IDs
     var pair = new KeyValuePair<int, int>(CAMERA_ID, id);
     Target target = client.listener.GetTarget(pair);
 
+    deltaTime += Time.deltaTime;
+
     transform.position = target.GetPosition();
     transform.rotation = target.GetRotation();
+
+    // Reset delta time if the data changed
+    if (!target.Equals(lastTarget)) deltaTime = 0;
+
+    lastTarget = target;
   }
 }
