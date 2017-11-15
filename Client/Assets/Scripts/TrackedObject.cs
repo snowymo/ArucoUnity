bae07c632edc4089af23231c41c1717bc5892912
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-15 16:43:42 -0500

    Smooth TrackedObject position with Holojam adaptive smoother

--- Client/Assets/Scripts/TrackedObject.cs
+++ Client/Assets/Scripts/TrackedObject.cs
@@ -11,6 +11,10 @@ public class TrackedObject : MonoBehaviour {
   Target lastTarget;
   float deltaTime = 0;
 
+  Holojam.Utility.AdaptiveSmoother smoother
+    = new Holojam.Utility.AdaptiveSmoother();
+  Vector3 lastPosition;
+
   void Update() {
     // Get target from listener with camera and target IDs
     var pair = new KeyValuePair<int, int>(CAMERA_ID, id);
@@ -18,7 +22,12 @@ public class TrackedObject : MonoBehaviour {
 
     deltaTime += Time.deltaTime;
 
-    transform.position = target.GetPosition();
+    transform.position = smoother.Smooth(
+      target.GetPosition(),
+      ref lastPosition,
+      deltaTime
+    );
+
     transform.rotation = target.GetRotation();
 
     // Reset delta time if the data changed
